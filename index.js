010b5080618f70515b65e32f65dbcc16eeb7284b
ana.volaric95@gmail.com <ana.volaric95@gmail.com>
2018-08-24 08:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 
-function happyHolidaysTo() {
-  
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo() {
